20663b2e704491a0c11b429e8efeb90f9afb101a
tigerenergy <kisoon0947@gmail.com>
2022-05-11 09:29:11 +0900

    update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import mealsImage from '../../assets/meals.jpg'
 import classes from './Header.module.css'
+import Button from './HeaderCartButton'
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <Button />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='good food' />
